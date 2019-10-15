8052374bb795d1b2615aa461c2283d70d4613525
Andresa Martins <contact@andresa.dev>
2019-10-15 19:02:20 -0300

    Poupup Display Module Registration

---
+++ popup-display/registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Module Registration
+ *
+ * @category   Prestafy
+ * @package    Prestafy_PopupDisplay
+ * @author     Andresa Martins <contact@andresa.dev>
+ * @copyright  Copyright (c) 2019 Prestafy eCommerce Solutions (https://www.prestafy.com.br)
+ * @license    http://opensource.org/licenses/BSD-2-Clause Simplified BSD License
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Prestafy_PopupDisplay',
+    __DIR__);
